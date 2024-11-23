87f04ac2c05f520b93621d004ac73ad6825f66f5
Ranjit hawee <ronjithawee2017@gmail.com>
2024-11-23 18:20:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+// jquery code
+$(function () {
+  
+
+// counter jquery pluggin
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+      
+
+});
+
+
+// js code
+new WOW().init();
+ 
+// glide js pluggin activation
+new Glide('.glide', {
+    type: "carousel",
+    autoplay: 3000,
+    perView: 1
+}).mount();
+
+
+
+
+
+
+
+
+
+
